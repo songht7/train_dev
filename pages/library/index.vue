65af476949bb20c1ecd3cecf0e520e9e3f626981
songht7 <leon.ting@okkuaixiu.com>
2019-03-30 23:35:32 +0800

    +

--- pages/library/index.vue
+++ pages/library/index.vue
@@ -1,15 +1,15 @@
 <template>
 	<view class="library">
 		<view class="page-main">
-			<view class="lib-row" v-for="(r,k) in 10" :key="k" @click="goDetail(r)">
-				<view class="lib-block">
-					<view class="lib-title">如何写出一份优秀的个人简历</view>
-					<view class="lib-more">
-						<view class="lib-m-left">
-							<view class="lib-ov">抱歉 这是专业HR都不会告诉你的两条简历制作方法 先和大家说一句...</view>
-							<view class="lib-browse">1896 浏览</view>
+			<view class="list-row" v-for="(r,k) in 10" :key="k" @click="goDetail(r)">
+				<view class="list-block">
+					<view class="list-title">如何写出一份优秀的个人简历</view>
+					<view class="list-more">
+						<view class="list-left">
+							<view class="list-ov">抱歉 这是专业HR都不会告诉你的两条简历制作方法 先和大家说一句...</view>
+							<view class="list-browse">1896 浏览</view>
 						</view>
-						<view class="lib-m-right">
+						<view class="list-right">
 							<image class="image-full" :src="sourceUrl+'/data/image_doc/6aa5e95da760264b14d7e73618693e74.jpg'" mode="aspectFill"></image>
 						</view>
 					</view>
@@ -45,57 +45,4 @@
 </script>
 
 <style>
-	.lib-row {
-		background: #FFFFFF;
-		margin-bottom: 30upx;
-	}
-
-	.lib-block {
-		padding: 30upx 20upx;
-	}
-
-	.lib-block,
-	.lib-more {
-		display: flex;
-		justify-content: flex-start;
-		align-content: center;
-		flex-direction: column;
-	}
-
-	.lib-more {
-		flex-direction: row;
-		justify-content: space-between;
-	}
-
-	.lib-title {
-		color: #222222;
-		font-size: 34upx;
-		line-height: 1.6;
-		padding-bottom: 10upx;
-	}
-
-	.lib-more {
-		color: #929292;
-		font-size: 26upx;
-		line-height: 1.4;
-	}
-
-	.lib-ov {
-		line-height: 1.4;
-		padding-bottom: 20upx;
-	}
-
-	.lib-m-left {
-		display: flex;
-		align-content: space-between;
-		flex: 1;
-		flex-direction: column;
-	}
-
-	.lib-m-right {
-		width: 30%;
-		height: 134upx;
-		border-radius: 8upx;
-		overflow: hidden;
-	}
 </style>
--- pages/user/index.vue
+++ pages/user/index.vue
@@ -7,7 +7,7 @@
 					<image class="user-portrait" v-show="$store.state.user.Portrait" :src="$store.state.user.Portrait" mode="aspectFill"></image>
 				</view>
 				<view class="user-infos">
-					<view class="user-name">{{UserId?$store.state.user.UserName:"用户名"}}</view>
+					<view class="user-name txt-sross">{{UserId?$store.state.user.UserName:"用户名"}}</view>
 					<view class="user-more my-collection"><text>我的收藏</text></view>
 					<view class="user-more my-resume"><text>我的简历</text></view>
 				</view>
@@ -30,6 +30,39 @@
 					<view class="my-class-block">
 						<view class="class-count class-state-red">2</view>
 						<view class="class-overview">未通过考试</view>
+					</view>
+				</view>
+			</view>
+		</view>
+		<view class="user-block">
+			<view class="user-class-list">
+				<view class="my-class-head">
+					<view class="class-tip">
+						<view class="class-icon">
+							<uni-icon type="shuji" :size="20" color="#FFFFFF"></uni-icon>
+						</view>
+						<view class="txt-sross">我参与的课程</view>
+					</view>
+					<view class="class-more">全部8个></view>
+				</view>
+				<view class="class-list">
+					<view class="list-row class-list-row" v-for="(r,k) in 3" :key="k">
+						<view class="list-block">
+							<view class="list-more">
+								<view class="list-left class-list-left">
+									<view class="list-title">质检员基础知识培训课程{{k}}</view>
+									<view class="class-progress">
+										<view class="progress-box">
+											<view class="percent">{{k==2?"开始学习":"已学60%"}}</view>
+											<progress :percent="k==2?'0':'60'" stroke-width="4" activeColor="#008CEE" backgroundColor="#E0E0E0"/>
+										</view>
+									</view>
+								</view>
+								<view class="list-right">
+									<image class="image-full" :src="sourceUrl+'/data/image_doc/6aa5e95da760264b14d7e73618693e74.jpg'" mode="aspectFill"></image>
+								</view>
+							</view>
+						</view>
 					</view>
 				</view>
 			</view>
@@ -101,9 +134,6 @@
 	}
 
 	.user-name {
-		font-size: 34upx;
-		font-weight: 500;
-		color: #222222;
 		line-height: 2;
 		width: 100%;
 	}
@@ -171,4 +201,61 @@
 	.class-state-red {
 		color: #EE6858;
 	}
+
+	.my-class-head {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-content: center;
+		align-items: center;
+	}
+
+	.class-tip {
+		display: flex;
+		flex-direction: row;
+		justify-content: flex-start;
+		align-content: center;
+		align-items: center;
+	}
+
+	.class-icon {
+		background: linear-gradient(#48AFBE, #58D3BE);
+		width: 50upx;
+		height: 50upx;
+		display: flex;
+		justify-content: center;
+		align-content: center;
+		align-items: center;
+		border-radius: 50%;
+		overflow: hidden;
+		margin-right: 10upx;
+	}
+
+	.class-more {
+		font-size: 28upx;
+		color: #929292;
+	}
+
+	.class-list-row {
+		border-bottom: none;
+	}
+
+	.class-list-left {
+		display: flex;
+		flex-direction: column;
+		align-content: space-between;
+		align-items: flex-start;
+		height: 134upx;
+	}
+
+	.class-progress {
+		width: 95%;
+	}
+
+	.progress-box {
+		display: flex;
+		flex-direction: column;
+		height: 50upx;
+	}
+	.percent{text-align: right;}
 </style>
